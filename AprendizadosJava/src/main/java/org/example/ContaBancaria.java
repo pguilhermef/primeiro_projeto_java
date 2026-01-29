dff43e766104d24d1939f4ce88e6479fe40c149d
Paulo Guilherme Fidelis dos Santos <pguilherme.fidelis@sysmanager.com.br>
2026-01-29 09:27:09 -0300

    Trata erros de metodos sendo chamados no lugar errado

--- AprendizadosJava/src/main/java/org/example/ContaBancaria.java
+++ AprendizadosJava/src/main/java/org/example/ContaBancaria.java
@@ -1,72 +1,36 @@
 package org.example;
 
 import java.math.BigDecimal;
-import java.util.Scanner;
 
 public class ContaBancaria {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String titular;
-        BigDecimal saldo = new BigDecimal("0.0");
-        boolean appEmExecucao = true;
-        Integer opcaoSelecionada;
+    private final String titular;
+    private BigDecimal saldo;
 
-        while(appEmExecucao){
-            System.out.println("""
-            Bem vindo ao Java Banking!
-            
-            Selecione uma opção abaixo e faça suas transações!
-            
-            1 - Depositar
-            2 - Sacar
-            3 - Ver Saldo
-            4 - Sair
-            """);
+    public ContaBancaria(String titular){
+        this.titular = titular;
+        this.saldo = BigDecimal.ZERO;
+    };
 
-            opcaoSelecionada = scanner.nextInt();
-
-            switch(opcaoSelecionada){
-                case 1:
-                    depositar();
-                case 2:
-                    sacar();
-                case 3:
-                    getSaldo();
-                case 4:
-                    System.out.println("Agradecemos seu uso. Até logo!");
-                    appEmExecucao = false;
-                default:
-                    System.out.println("Selecione uma opção de 1-4.");
-            }
-
-        }
+    public void depositar(BigDecimal valor){
+        ValidarValor(valor);
+        saldo = saldo.add(valor);
     }
 
-    public void depositar(){
-        System.out.println("Digite um valor para depositar em sua conta:");
-        BigDecimal valor = scanner.nextBigDecimal();
-
-        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0){
-            throw new IllegalArgumentException("Não é possível adicionar valores negativos.");
-        } else {
-            saldo = saldo.add(valor);
-            System.out.printf("Você retirou um total de R$%s\n", valor);
+    public void sacar(BigDecimal valor){
+        ValidarValor(valor);
+        if (saldo.compareTo(valor) < 0){
+            throw new IllegalArgumentException("O valor de saque é maior do que o valor disponível.");
         }
-    }
-
-    public void sacar(){
-        System.out.println("Digite um valor para sacar em sua conta:");
-        BigDecimal valor = scanner.nextBigDecimal();
-
-        if(valor == null || valor.compareTo(BigDecimal.ZERO) <= 0){
-            throw new IllegalArgumentException("Não é possível sacar valores negativos.");
-        } else {
-            saldo = saldo.subtract(valor);
-            System.out.printf("Você retirou um total de R$%s\n", valor);
-        }
+        saldo = saldo.subtract(valor);
     }
 
     public void getSaldo(){
-        System.out.println("Seu total de saldo em conta é: " + saldo);
+        System.out.printf("Seu saldo é R$%s", saldo);;
+    };
+
+    private void ValidarValor(BigDecimal valor){
+        if(valor == null || valor.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("Valor deve ser positivo.");
+        }
     }
 }
--- AprendizadosJava/src/main/java/org/example/Main.java
+++ AprendizadosJava/src/main/java/org/example/Main.java
@@ -1,17 +1,63 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String titular;
+        boolean appEmExecucao = true;
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Digite seu nome e acesse sua conta:");
+        titular = scanner.nextLine();
+
+        ContaBancaria contaUm = new ContaBancaria(titular);
+
+        System.out.printf("Bem vindo, %s\n", titular);
+
+        while(appEmExecucao){
+            int opcaoSelecionada = 0;
+            BigDecimal valorDigitado;
+
+            System.out.println("""
+            
+            --- Java Banking ---
+            
+            Selecione uma opção abaixo e faça suas transações!
+            
+            1 - Depositar
+            2 - Sacar
+            3 - Ver Saldo
+            4 - Sair
+            """);
+
+            opcaoSelecionada = scanner.nextInt();
+
+            switch(opcaoSelecionada){
+                case 1:
+                    System.out.println("Digite o valor a ser depositado no formato 0,0:");
+                    valorDigitado = scanner.nextBigDecimal();
+                    contaUm.depositar(valorDigitado);
+                    break;
+                case 2:
+                    System.out.println("Digite o valor a ser sacado no formato 0,0:");
+                    valorDigitado = scanner.nextBigDecimal();
+                    contaUm.sacar(valorDigitado);
+                    break;
+                case 3:
+                    contaUm.getSaldo();
+                    break;
+                case 4:
+                    System.out.println("Agradecemos seu uso. Até logo!");
+                    appEmExecucao = false;
+                    break;
+                default:
+                    System.out.println("Selecione uma opção de 1-4.");
+            }
+
         }
     }
 }
